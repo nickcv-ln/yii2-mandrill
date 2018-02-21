8111fb43b771cce6b27e972b05210adb2adbe2bd
simialbi <simi.albi@gmail.com>
2018-02-21 08:41:54 +0100

    applied repository code style

--- src/Mailer.php
+++ src/Mailer.php
@@ -23,221 +23,229 @@ use Mandrill_Error;
  * @author Nicola Puddu <n.puddu@outlook.com>
  * @version 1.0
  */
-class Mailer extends BaseMailer {
+class Mailer extends BaseMailer
+{
 
-	const STATUS_SENT = 'sent';
-	const STATUS_QUEUED = 'queued';
-	const STATUS_SCHEDULED = 'scheduled';
-	const STATUS_REJECTED = 'rejected';
-	const STATUS_INVALID = 'invalid';
-	const LOG_CATEGORY = 'mandrill';
+    const STATUS_SENT = 'sent';
+    const STATUS_QUEUED = 'queued';
+    const STATUS_SCHEDULED = 'scheduled';
+    const STATUS_REJECTED = 'rejected';
+    const STATUS_INVALID = 'invalid';
+    const LOG_CATEGORY = 'mandrill';
 
-	const LANGUAGE_MAILCHIMP = 'mailchimp';
-	const LANGUAGE_HANDLEBARS = 'handlebars';
+    const LANGUAGE_MAILCHIMP = 'mailchimp';
+    const LANGUAGE_HANDLEBARS = 'handlebars';
 
-	/**
-	 * @var string Mandrill API key
-	 */
-	private $_apikey;
+    /**
+     * @var string Mandrill API key
+     */
+    private $_apikey;
 
-	/**
-	 * Whether the mailer should use mandrill templates instead of Yii views.
-	 *
-	 * @var boolean use mandrill templates instead of Yii views.
-	 * @since 1.2.0
-	 */
-	public $useMandrillTemplates = false;
+    /**
+     * Whether the mailer should use mandrill templates instead of Yii views.
+     *
+     * @var boolean use mandrill templates instead of Yii views.
+     * @since 1.2.0
+     */
+    public $useMandrillTemplates = false;
 
-	/**
-	 * Whether the mailer should use the template defaults when using mandrill
-	 * templates.
-	 *
-	 * @var boolean use mandrill template defaults.
-	 * @since 1.4.0
-	 */
-	public $useTemplateDefaults = true;
+    /**
+     * Whether the mailer should use the template defaults when using mandrill
+     * templates.
+     *
+     * @var boolean use mandrill template defaults.
+     * @since 1.4.0
+     */
+    public $useTemplateDefaults = true;
 
-	/**
-	 * What language is used in mandrill templates, either mailchimp or handlebars
-	 * Mailchimp language allows to use mc:edit and *|VAR|*
-	 * Handlebars language allows to use {{ var }}, loops, conditions @link http://handlebarsjs.com/
-	 *
-	 * @var string language, that is used in templates
-	 * @since 1.5.0
-	 */
-	public $templateLanguage = self::LANGUAGE_MAILCHIMP;
+    /**
+     * What language is used in mandrill templates, either mailchimp or handlebars
+     * Mailchimp language allows to use mc:edit and *|VAR|*
+     * Handlebars language allows to use {{ var }}, loops, conditions @link http://handlebarsjs.com/
+     *
+     * @var string language, that is used in templates
+     * @since 1.5.0
+     */
+    public $templateLanguage = self::LANGUAGE_MAILCHIMP;
 
-	/**
-	 * @var string message default class name.
-	 */
-	public $messageClass = 'nickcv\mandrill\Message';
+    /**
+     * @var string message default class name.
+     */
+    public $messageClass = '\nickcv\mandrill\Message';
 
-	/**
-	 * @var Mandrill the Mandrill instance
-	 */
-	private $_mandrill;
+    /**
+     * @var Mandrill the Mandrill instance
+     */
+    private $_mandrill;
 
-	/**
-	 * @var array last response from mandrill
-	 * @since 1.7.0
-	 */
-	private $_mandrillResponse = [];
+    /**
+     * @var array last response from mandrill
+     * @since 1.7.0
+     */
+    private $_mandrillResponse = [];
 
-	/**
-	 * Checks that the API key has indeed been set.
-	 *
-	 * @inheritdoc
-	 */
-	public function init() {
-		if (!$this->_apikey) {
-			throw new InvalidConfigException('"'.get_class($this).'::apikey" cannot be null.');
-		}
+    /**
+     * Checks that the API key has indeed been set.
+     *
+     * @inheritdoc
+     */
+    public function init()
+    {
+        if (!$this->_apikey) {
+            throw new InvalidConfigException('"' . get_class($this) . '::apikey" cannot be null.');
+        }
 
-		if (!in_array($this->templateLanguage, [self::LANGUAGE_MAILCHIMP, self::LANGUAGE_HANDLEBARS])) {
-			throw new InvalidConfigException('"'.get_class($this).'::templateLanguage" has an invalid value.');
-		}
+        if (!in_array($this->templateLanguage, [self::LANGUAGE_MAILCHIMP, self::LANGUAGE_HANDLEBARS])) {
+            throw new InvalidConfigException('"' . get_class($this) . '::templateLanguage" has an invalid value.');
+        }
 
-		try {
-			$this->_mandrill = new Mandrill($this->_apikey);
-		} catch (\Exception $exc) {
-			\Yii::error($exc->getMessage());
-			throw new \Exception('an error occurred with your mailer. Please check the application logs.', 500);
-		}
-	}
+        try {
+            $this->_mandrill = new Mandrill($this->_apikey);
+        } catch (\Exception $exc) {
+            \Yii::error($exc->getMessage());
+            throw new \Exception('an error occurred with your mailer. Please check the application logs.', 500);
+        }
+    }
 
-	/**
-	 * Sets the API key for Mandrill
-	 *
-	 * @param string $apikey the Mandrill API key
-	 *
-	 * @throws InvalidConfigException
-	 */
-	public function setApikey($apikey) {
-		if (!is_string($apikey)) {
-			throw new InvalidConfigException('"'.get_class($this).'::apikey" should be a string, "'.gettype($apikey).'" given.');
-		}
+    /**
+     * Sets the API key for Mandrill
+     *
+     * @param string $apikey the Mandrill API key
+     *
+     * @throws InvalidConfigException
+     */
+    public function setApikey($apikey)
+    {
+        if (!is_string($apikey)) {
+            throw new InvalidConfigException('"' . get_class($this) . '::apikey" should be a string, "' . gettype($apikey) . '" given.');
+        }
 
-		$trimmedApikey = trim($apikey);
-		if (!strlen($trimmedApikey) > 0) {
-			throw new InvalidConfigException('"'.get_class($this).'::apikey" length should be greater than 0.');
-		}
+        $trimmedApikey = trim($apikey);
+        if (!strlen($trimmedApikey) > 0) {
+            throw new InvalidConfigException('"' . get_class($this) . '::apikey" length should be greater than 0.');
+        }
 
-		$this->_apikey = $trimmedApikey;
-	}
+        $this->_apikey = $trimmedApikey;
+    }
 
-	/**
-	 * Gets Mandrill instance
-	 *
-	 * @return Mandrill initialized Mandrill
-	 * @since 1.6.0
-	 */
-	public function getMandrill() {
-		return $this->_mandrill;
-	}
+    /**
+     * Gets Mandrill instance
+     *
+     * @return Mandrill initialized Mandrill
+     * @since 1.6.0
+     */
+    public function getMandrill()
+    {
+        return $this->_mandrill;
+    }
 
-	/**
-	 * Returns the array of the last transaction returned by Mandrill.
-	 *
-	 * @return array
-	 * @since 1.7.0
-	 */
-	public function getLastTransaction() {
-		return $this->_mandrillResponse;
-	}
+    /**
+     * Returns the array of the last transaction returned by Mandrill.
+     *
+     * @return array
+     * @since 1.7.0
+     */
+    public function getLastTransaction()
+    {
+        return $this->_mandrillResponse;
+    }
 
-	/**
-	 * Composes the message using a Mandrill template if the useMandrillTemplates
-	 * settings is true.
-	 *
-	 * If mandrill templates are not being used or if no template with the given
-	 * name has been found it will fallback to the normal compose method.
-	 *
-	 * @inheritdoc
-	 * @since 1.2.0
-	 */
-	public function compose($view = null, array $params = []) {
-		if ($this->useMandrillTemplates) {
-			/** @var Message $message */
-			$message = parent::compose();
-			$message->setTemplateData($view, $params, $this->templateLanguage);
-			if ($this->useTemplateDefaults) {
-				$message->enableTemplateDefaults();
-			}
+    /**
+     * Composes the message using a Mandrill template if the useMandrillTemplates
+     * settings is true.
+     *
+     * If mandrill templates are not being used or if no template with the given
+     * name has been found it will fallback to the normal compose method.
+     *
+     * @inheritdoc
+     * @since 1.2.0
+     */
+    public function compose($view = null, array $params = [])
+    {
+        if ($this->useMandrillTemplates) {
+            /** @var Message $message */
+            $message = parent::compose();
+            $message->setTemplateData($view, $params, $this->templateLanguage);
+            if ($this->useTemplateDefaults) {
+                $message->enableTemplateDefaults();
+            }
 
-			return $message;
-		}
+            return $message;
+        }
 
-		return parent::compose($view, $params);
-	}
+        return parent::compose($view, $params);
+    }
 
-	/**
-	 * Sends the specified message.
-	 *
-	 * @param Message $message the message to be sent
-	 *
-	 * @return boolean whether the message is sent successfully
-	 */
-	protected function sendMessage($message) {
-		\Yii::info('Sending email "'.$message->getSubject().'" to "'.implode(', ', $message->getTo()).'"', self::LOG_CATEGORY);
+    /**
+     * Sends the specified message.
+     *
+     * @param Message $message the message to be sent
+     *
+     * @return boolean whether the message is sent successfully
+     */
+    protected function sendMessage($message)
+    {
+        \Yii::info('Sending email "' . $message->getSubject() . '" to "' . implode(', ', $message->getTo()) . '"', self::LOG_CATEGORY);
 
-		try {
-			if ($this->useMandrillTemplates) {
-				return $this->wasMessageSentSuccesfully(
-					$this->_mandrill->messages->sendTemplate(
-						$message->getTemplateName(),
-						$message->getTemplateContent(),
-						$message->getMandrillMessageArray(),
-						$message->isAsync()
-					)
-				);
-			} else {
-				return $this->wasMessageSentSuccesfully(
-					$this->_mandrill->messages->send(
-						$message->getMandrillMessageArray(),
-						$message->isAsync()
-					)
-				);
-			}
-		} catch (Mandrill_Error $e) {
-			\Yii::error('A mandrill error occurred: '.get_class($e).' - '.$e->getMessage(), self::LOG_CATEGORY);
+        try {
+            if ($this->useMandrillTemplates) {
+                return $this->wasMessageSentSuccesfully(
+                    $this->_mandrill->messages->sendTemplate(
+                        $message->getTemplateName(),
+                        $message->getTemplateContent(),
+                        $message->getMandrillMessageArray(),
+                        $message->isAsync()
+                    )
+                );
+            } else {
+                return $this->wasMessageSentSuccesfully(
+                    $this->_mandrill->messages->send(
+                        $message->getMandrillMessageArray(),
+                        $message->isAsync()
+                    )
+                );
+            }
+        } catch (Mandrill_Error $e) {
+            \Yii::error('A mandrill error occurred: ' . get_class($e) . ' - ' . $e->getMessage(), self::LOG_CATEGORY);
 
-			return false;
-		}
-	}
+            return false;
+        }
+    }
 
-	/**
-	 * parse the mandrill response and returns false if any message was either invalid or rejected
-	 *
-	 * @param array $mandrillResponse
-	 *
-	 * @return boolean
-	 */
-	private function wasMessageSentSuccesfully($mandrillResponse) {
-		$this->_mandrillResponse = $mandrillResponse;
+    /**
+     * parse the mandrill response and returns false if any message was either invalid or rejected
+     *
+     * @param array $mandrillResponse
+     *
+     * @return boolean
+     */
+    private function wasMessageSentSuccesfully($mandrillResponse)
+    {
+        $this->_mandrillResponse = $mandrillResponse;
 
-		$return = true;
-		foreach ($mandrillResponse as $recipient) {
-			switch ($recipient['status']) {
-				case self::STATUS_INVALID:
-					$return = false;
-					\Yii::warning('the email for "'.$recipient['email'].'" has not been sent: status "'.$recipient['status'].'"', self::LOG_CATEGORY);
-					break;
-				case self::STATUS_QUEUED:
-					\Yii::info('the email for "'.$recipient['email'].'" is now in a queue waiting to be sent.', self::LOG_CATEGORY);
-					break;
-				case self::STATUS_REJECTED:
-					$return = false;
-					\Yii::warning('the email for "'.$recipient['email'].'" has been rejected: reason "'.$recipient['reject_reason'].'"', self::LOG_CATEGORY);
-					break;
-				case self::STATUS_SCHEDULED:
-					\Yii::info('the email submission for "'.$recipient['email'].'" has been scheduled.', self::LOG_CATEGORY);
-					break;
-				case self::STATUS_SENT:
-					\Yii::info('the email for "'.$recipient['email'].'" has been sent.', self::LOG_CATEGORY);
-					break;
-			}
-		}
+        $return = true;
+        foreach ($mandrillResponse as $recipient) {
+            switch ($recipient['status']) {
+                case self::STATUS_INVALID:
+                    $return = false;
+                    \Yii::warning('the email for "' . $recipient['email'] . '" has not been sent: status "' . $recipient['status'] . '"', self::LOG_CATEGORY);
+                    break;
+                case self::STATUS_QUEUED:
+                    \Yii::info('the email for "' . $recipient['email'] . '" is now in a queue waiting to be sent.', self::LOG_CATEGORY);
+                    break;
+                case self::STATUS_REJECTED:
+                    $return = false;
+                    \Yii::warning('the email for "' . $recipient['email'] . '" has been rejected: reason "' . $recipient['reject_reason'] . '"', self::LOG_CATEGORY);
+                    break;
+                case self::STATUS_SCHEDULED:
+                    \Yii::info('the email submission for "' . $recipient['email'] . '" has been scheduled.', self::LOG_CATEGORY);
+                    break;
+                case self::STATUS_SENT:
+                    \Yii::info('the email for "' . $recipient['email'] . '" has been sent.', self::LOG_CATEGORY);
+                    break;
+            }
+        }
 
-		return $return;
-	}
+        return $return;
+    }
 }
